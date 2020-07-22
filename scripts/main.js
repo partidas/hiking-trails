3904bee0cc37bc25f58ee656dedf9bcad5622ba6
Steven Partida <ste94par@gmail.com>
2020-07-22 16:21:20 -0700

    Adding more Javascript features and a new image.
    Adding an image changer to switch between 2 images. 2nd image
    added as well.

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,30 @@
 const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+if(myHeading.textContent === "NATURE, Dont you love it? ") {
+    alert('Switching Heading!');
+    myHeading.textContent = 'Hello world!';
+}
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/nature-background-mountian-riverside-preview.jpg') {
+        myImage.setAttribute ('src', 'images/sequoia.jpeg');
+    } else {
+        myImage.setAttribute ('src', 'images/nature-background-mountian-riverside-preview.jpg');
+    }
+}
+
+/*
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
+
+/*
+function multiply(num1,num2) {
+    let result = num1 * num2;
+    return result;
+}
+
+alert(multiply(5,5));
+*/
